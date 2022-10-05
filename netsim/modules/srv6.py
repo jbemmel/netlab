6f6ef9b1a2b9c5db311d277c6ceb90476c2422ce
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-10-05 08:07:34 -0500

    overlaps -> in

--- netsim/modules/srv6.py
+++ netsim/modules/srv6.py
@@ -10,16 +10,13 @@ import netaddr
 class SRV6(_Module):
 
   def node_post_transform(self, node: Box, topology: Box) -> None:
-
-      print( f"JvB: srv6 node_post_transform {node.loopback}" )
-
       # Could model this as another addressing pool too
       locator = netaddr.IPNetwork( f'{topology.defaults.srv6.locator}:{node.id:x}::/64' )
 
       if 'ipv6' not in node.loopback:
           common.error( f"SRv6 requires an ipv6 loopback address on node {node.name}",
                         common.MissingValue, 'srv6' )
-      elif locator.overlaps( netaddr.IPNetwork(node.loopback.ipv6) ):
+      elif netaddr.IPNetwork(node.loopback.ipv6) in locator:
           common.error( f"Node {node.name} ipv6 loopback address {node.loopback.ipv6} overlaps with locator {locator}",
                         common.IncorrectValue, 'srv6' )
 
